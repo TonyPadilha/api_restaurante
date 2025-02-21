cb1ee8790d8b6dc1e49c556617e0391bca978367
TONY ANDREW PADILHA DA SILVA <04673316266@DPERO.local>
2025-02-21 12:02:00 -0400

    first commit

---
+++ modelos/app.py
@@ -0,0 +1,19 @@
+from classes.restaurante import Restaurante
+from classes.cardapio.bebida import Bebida
+from classes.cardapio.prato import Prato
+
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melância', 5.0, 'Grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho', 2.00, 'O Melhor Pão da Cidade')
+prato_paozinho.aplicar_desconto()
+
+restaurante_praca.adicionar_carpadio(bebida_suco)
+restaurante_praca.adicionar_carpadio(prato_paozinho)
+
+
+def main():
+    restaurante_praca.exibir_cardapio
+if __name__ == '__main__':
+    main()
+
